4fd8f18e1f446a8d98bfe2f0470a92a795756b15
Evan Nemerson <evan@coeus-group.com>
2013-08-14 12:05:10 -0700

    lzf: add internal liblzf copy to fall back on
    https://github.com/quixdb/squash/issues/19
    
    Signed-off-by: Evan Nemerson <evan@coeus-group.com>

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-for submodule in plugins/lz4/lz4; do
+for submodule in `grep -oP 'path = (.+)' .gitmodules | sed -E -e 's/^path = (.+)$/\1/'`; do
+  echo "Checking $submodule"
   if [ ! -d "${submodule}/.git" ]; then
     git submodule init
     git submodule update
