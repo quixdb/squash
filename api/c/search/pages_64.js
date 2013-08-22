71c36605ee1d3741ccb8533987e97564d0ba2070
Evan Nemerson <evan@coeus-group.com>
2013-08-22 15:47:17 -0700

    Update C API reference to 8f2631f4

---
+++ api/c/search/pages_64.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['doboz_20plugin',['doboz Plugin',['../md_doboz.html',1,'']]]
+];
--- api/c/search/pages_6c.js
+++ api/c/search/pages_6c.js
@@ -3,5 +3,6 @@ var searchData=
   ['lz4_20plugin',['lz4 Plugin',['../md_lz4.html',1,'']]],
   ['lzf_20plugin',['lzf Plugin',['../md_lzf.html',1,'']]],
   ['lzma_20plugin',['lzma Plugin',['../md_lzma.html',1,'']]],
+  ['lzmat_20plugin',['lzmat Plugin',['../md_lzmat.html',1,'']]],
   ['lzo_20plugin',['lzo Plugin',['../md_lzo.html',1,'']]]
 ];
