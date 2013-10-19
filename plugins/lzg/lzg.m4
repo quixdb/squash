4b0cae4091bbc97947864dd88fd2705cbbb75d96
Evan Nemerson <evan@coeus-group.com>
2013-10-19 16:23:54 -0700

    tests: Add random data test.
    This also disables the lzg plugin, as it fails this test.  Bug filed
    upstream at http://sourceforge.net/p/liblzg/bugs/2/
    
    Signed-off-by: Evan Nemerson <evan@coeus-group.com>

--- plugins/lzg/lzg.m4
+++ plugins/lzg/lzg.m4
@@ -1,5 +1,5 @@
 AC_ARG_ENABLE([lzg],
-              [AC_HELP_STRING([--enable-lzg=@<:@yes/no@:>@], [Enable lzg plugin @<:@default=yes@:>@])],,
-              [enable_lzg=yes])
+              [AC_HELP_STRING([--enable-lzg=@<:@yes/no@:>@], [Enable lzg plugin @<:@default=no@:>@])],,
+              [enable_lzg=no])
 
 AM_CONDITIONAL([ENABLE_LZG], test x$enable_lzg = "xyes")
--- tests/check-plugins.c
+++ tests/check-plugins.c
@@ -268,6 +268,55 @@ check_stream_decompress (gconstpointer user_data) {
   free (decompressed);
 }
 
+static void
+check_random_data (gconstpointer user_data) {
+  SquashCodec* codec = (SquashCodec*) user_data;
+  const size_t max_uncompressed_length = 4096;
+  size_t uncompressed_length;
+  uint8_t* uncompressed_data = (uint8_t*) malloc (max_uncompressed_length);
+  size_t compressed_length;
+  uint8_t* compressed_data = (uint8_t*) malloc (squash_codec_get_max_compressed_size (codec, max_uncompressed_length));
+  size_t decompressed_length;
+  uint8_t* decompressed_data = (uint8_t*) malloc (max_uncompressed_length);
+
+  size_t uncompressed_data_filled = 0;
+  SquashStatus res;
+
+  for ( uncompressed_length = 32 ; uncompressed_length <= max_uncompressed_length ; uncompressed_length += g_test_rand_int_range (32, 128) ) {
+    for ( ; uncompressed_data_filled < uncompressed_length ; uncompressed_data_filled++ ) {
+      uncompressed_data[uncompressed_data_filled] = (uint8_t) g_test_rand_int_range (0x00, 0xff);
+    }
+
+    compressed_length = squash_codec_get_max_compressed_size (codec, uncompressed_length);
+    g_assert (compressed_length > 0);
+
+    res = squash_codec_compress_with_options (codec, compressed_data, &compressed_length, (uint8_t*) uncompressed_data, uncompressed_length, NULL);
+    SQUASH_ASSERT_OK(res);
+    g_assert (compressed_length > 0);
+
+    decompressed_length = uncompressed_length;
+    res = squash_codec_decompress_with_options (codec, decompressed_data, &decompressed_length, compressed_data, compressed_length, NULL);
+    SQUASH_ASSERT_OK(res);
+    g_assert (decompressed_length == uncompressed_length);
+
+    int memcmpres = memcmp (uncompressed_data, decompressed_data, uncompressed_length);
+    if (memcmpres != 0) {
+      size_t pos;
+      for (pos = 0 ; pos < decompressed_length ; pos++ ) {
+        if (uncompressed_data[pos] != decompressed_data[pos]) {
+          fprintf (stderr, "failed at %zu of %zu\n", pos, decompressed_length);
+          break;
+        }
+      }
+    }
+    g_assert (memcmp (uncompressed_data, decompressed_data, uncompressed_length) == 0);
+  }
+
+  free (uncompressed_data);
+  free (compressed_data);
+  free (decompressed_data);
+}
+
 static void squash_check_codec_enumerator_codec_cb (SquashCodec* codec, void* data) {
   gchar* test_name = NULL;
 
@@ -288,6 +337,10 @@ static void squash_check_codec_enumerator_codec_cb (SquashCodec* codec, void* da
 
   test_name = g_strdup_printf ("/plugins/%s/stream/decompress", squash_codec_get_name (codec));
   g_test_add_data_func (test_name, codec, check_stream_decompress);
+  g_free (test_name);
+
+  test_name = g_strdup_printf ("/plugins/%s/random-data", squash_codec_get_name (codec));
+  g_test_add_data_func (test_name, codec, check_random_data);
   g_free (test_name);
 }
 
