044487c853df361b8fd7098db9a36522d1294692
Evan Nemerson <evan@coeus-group.com>
2013-10-19 11:59:43 -0700

    autogen.sh: replace per-submodule git submodule checks
    Signed-off-by: Evan Nemerson <evan@coeus-group.com>

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-for submodule in `grep -oE 'path = (.+)' .gitmodules | sed -E -e 's/^path = (.+)$/\1/'`; do
-  echo "Checking $submodule"
-  if [ ! -d "${submodule}/.git" ]; then
-    git submodule init
-    git submodule update
-  fi
-done
-
+git submodule update --init --recursive
 ./gitlog-to-changelog > ChangeLog
 autoreconf -iv || exit 1
 ./configure --enable-maintainer-mode "$@"
