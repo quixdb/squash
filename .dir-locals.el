404a4d682a40c68129a3ad00922774bcb6c5de80
Evan Nemerson <evan@nemerson.com>
2014-03-20 19:39:34 -0700

    emacs: add flags for ac-complete-clang

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,10 @@
 ((nil . ((indent-tabs-mode . nil)
          (c-basic-offset . 2)
          (tab-width . 2))))
+
+(setq
+ ac-clang-flags
+ (mapconcat
+  (function (lambda (x) (format "-I%s" x)))
+  (list (file-name-directory (dir-locals-find-file ".")))
+  " "))
