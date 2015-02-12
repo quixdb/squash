00a9c8b8380e102df533490393e7696017976d9a
Evan Nemerson <evan@nemerson.com>
2015-02-12 14:15:18 -0800

    ms-compress: update to latest git
    Signed-off-by: Evan Nemerson <evan@nemerson.com>

--- plugins/ms-compress/squash-ms-compress.c
+++ plugins/ms-compress/squash-ms-compress.c
@@ -32,93 +32,170 @@
 
 #include <squash/squash.h>
 
-#include "ms-compress/compression.h"
+#include "ms-compress/include/mscomp.h"
 
 SquashStatus squash_plugin_init_codec (SquashCodec* codec, SquashCodecFuncs* funcs);
 
-static CompressionFormat
-squash_codec_to_ms_compress_format (SquashCodec* codec) {
+typedef struct SquashMSCompStream_s {
+  SquashStream base_object;
+
+  mscomp_stream mscomp;
+} SquashMSCompStream;
+
+SquashStatus                squash_plugin_init_codec  (SquashCodec* codec, SquashCodecFuncs* funcs);
+
+static void                 squash_ms_stream_init     (SquashMSCompStream* stream,
+                                                       SquashCodec* codec,
+                                                       SquashStreamType stream_type,
+                                                       SquashOptions* options,
+                                                       SquashDestroyNotify destroy_notify);
+static SquashMSCompStream*  squash_ms_stream_new      (SquashCodec* codec, SquashStreamType stream_type, SquashOptions* options);
+static void                 squash_ms_stream_destroy  (void* stream);
+static void                 squash_ms_stream_free     (void* stream);
+
+static MSCompFormat
+squash_ms_format_from_codec (SquashCodec* codec) {
   const char* name = squash_codec_get_name (codec);
 
   if (name[5] == 0)
-    return COMPRESSION_LZNT1;
+    return MSCOMP_LZNT1;
   else if (name[6] == 0)
-    return COMPRESSION_XPRESS;
+    return MSCOMP_XPRESS;
   else if (name[14] == 0)
-    return COMPRESSION_XPRESS_HUFF;
+    return MSCOMP_XPRESS_HUFF;
   else
     assert (false);
 }
 
 static SquashStatus
-squash_ms_compress_compress_buffer (SquashCodec* codec,
-                                    uint8_t* compressed, size_t* compressed_length,
-                                    const uint8_t* uncompressed, size_t uncompressed_length,
-                                    SquashOptions* options) {
-  switch (squash_codec_to_ms_compress_format (codec)) {
-    case COMPRESSION_LZNT1:
-      *compressed_length = lznt1_compress ((const_bytes) uncompressed, uncompressed_length, (bytes) compressed, *compressed_length);
-      break;
-    case COMPRESSION_XPRESS:
-      *compressed_length = xpress_compress ((const_bytes) uncompressed, uncompressed_length, (bytes) compressed, *compressed_length);
-      break;
-    case COMPRESSION_XPRESS_HUFF:
-      *compressed_length = xpress_huff_compress ((const_bytes) uncompressed, uncompressed_length, (bytes) compressed, *compressed_length);
-      break;
+squash_ms_status_to_squash_status (MSCompStatus status) {
+  switch (status) {
+    case MSCOMP_OK:
+      return SQUASH_OK;
+    case MSCOMP_STREAM_END:
+      return SQUASH_END_OF_STREAM;
+    case MSCOMP_ERRNO:
+      return SQUASH_FAILED;
+    case MSCOMP_ARG_ERROR:
+      return SQUASH_BAD_PARAM;
+    case MSCOMP_DATA_ERROR:
+      return SQUASH_FAILED;
+    case MSCOMP_MEM_ERROR:
+      return SQUASH_MEMORY;
+    case MSCOMP_BUF_ERROR:
+      return SQUASH_FAILED;
+    default:
+      return SQUASH_FAILED;
+  }
+}
+
+static SquashMSCompStream*
+squash_ms_stream_new (SquashCodec* codec, SquashStreamType stream_type, SquashOptions* options) {
+  int lzham_e = 0;
+  SquashMSCompStream* stream;
+
+  assert (codec != NULL);
+  assert (stream_type == SQUASH_STREAM_COMPRESS || stream_type == SQUASH_STREAM_DECOMPRESS);
+
+  stream = malloc (sizeof (SquashMSCompStream));
+  squash_ms_stream_init (stream, codec, stream_type, options, squash_ms_stream_free);
+
+  return stream;
+}
+
+static void
+squash_ms_stream_init (SquashMSCompStream* stream,
+                       SquashCodec* codec,
+                       SquashStreamType stream_type,
+                       SquashOptions* options,
+                       SquashDestroyNotify destroy_notify) {
+  squash_stream_init ((SquashStream*) stream, codec, stream_type, options, destroy_notify);
+
+  MSCompStatus status;
+  MSCompFormat format = squash_ms_format_from_codec (codec);
+  if (stream->base_object.stream_type == SQUASH_STREAM_COMPRESS) {
+    status = ms_deflate_init (format, &(stream->mscomp));
+  } else {
+    status = ms_inflate_init (format, &(stream->mscomp));
+  }
+  assert (status == MSCOMP_OK);
+}
+
+static void
+squash_ms_stream_destroy (void* stream) {
+  SquashMSCompStream* s = (SquashMSCompStream*) stream;
+
+  if (s->base_object.stream_type == SQUASH_STREAM_COMPRESS) {
+    ms_deflate_end(&(s->mscomp));
+  } else {
+    ms_inflate_end(&(s->mscomp));
   }
 
-  return SQUASH_OK;
+  squash_stream_destroy (stream);
+}
+
+static void
+squash_ms_stream_free (void* stream) {
+  squash_ms_stream_destroy (stream);
+  free (stream);
+}
+
+static SquashStream*
+squash_ms_create_stream (SquashCodec* codec, SquashStreamType stream_type, SquashOptions* options) {
+  return (SquashStream*) squash_ms_stream_new (codec, stream_type, (SquashOptions*) options);
 }
 
 static SquashStatus
-squash_ms_compress_decompress_buffer (SquashCodec* codec,
+squash_ms_process_stream (SquashStream* stream, SquashOperation operation) {
+  MSCompStatus res;
+  SquashMSCompStream* s = (SquashMSCompStream*) stream;
+
+  s->mscomp.in = stream->next_in;
+  s->mscomp.in_avail = stream->avail_in;
+  s->mscomp.out = stream->next_out;
+  s->mscomp.out_avail = stream->avail_out;
+
+  if (stream->stream_type == SQUASH_STREAM_COMPRESS) {
+    res = ms_deflate(&(s->mscomp), operation == SQUASH_OPERATION_FINISH);
+  } else {
+    res = ms_inflate(&(s->mscomp), operation == SQUASH_OPERATION_FINISH);
+  }
+
+  stream->next_in = s->mscomp.in;
+  stream->avail_in = s->mscomp.in_avail;
+  stream->next_out = s->mscomp.out;
+  stream->avail_out = s->mscomp.out_avail;
+
+  if (res == MSCOMP_OK && operation == SQUASH_OPERATION_FINISH) {
+    return SQUASH_PROCESSING;
+  }
+
+  return squash_ms_status_to_squash_status (res);
+}
+
+static size_t
+squash_ms_get_max_compressed_size (SquashCodec* codec, size_t uncompressed_length) {
+  return ms_max_compressed_size (squash_ms_format_from_codec (codec), uncompressed_length);
+}
+
+static SquashStatus
+squash_ms_compress_buffer (SquashCodec* codec,
+                                    uint8_t* compressed, size_t* compressed_length,
+                                    const uint8_t* uncompressed, size_t uncompressed_length,
+                                    SquashOptions* options) {
+  MSCompStatus status = ms_compress (squash_ms_format_from_codec (codec),
+                                     uncompressed, uncompressed_length, compressed, compressed_length);
+  return squash_ms_status_to_squash_status (status);
+}
+
+static SquashStatus
+squash_ms_decompress_buffer (SquashCodec* codec,
                                       uint8_t* decompressed, size_t* decompressed_length,
                                       const uint8_t* compressed, size_t compressed_length,
                                       SquashOptions* options) {
-  switch (squash_codec_to_ms_compress_format (codec)) {
-    case COMPRESSION_LZNT1:
-      *decompressed_length = lznt1_decompress ((const_bytes) compressed, compressed_length, (bytes) decompressed, *decompressed_length);
-      break;
-    case COMPRESSION_XPRESS:
-      *decompressed_length = xpress_decompress ((const_bytes) compressed, compressed_length, (bytes) decompressed, *decompressed_length);
-      break;
-    case COMPRESSION_XPRESS_HUFF:
-      *decompressed_length = xpress_huff_decompress ((const_bytes) compressed, compressed_length, (bytes) decompressed, *decompressed_length);
-      break;
-  }
-
-  return SQUASH_OK;
-}
-
-static size_t
-squash_ms_compress_get_max_compressed_size (SquashCodec* codec, size_t uncompressed_length) {
-  switch (squash_codec_to_ms_compress_format (codec)) {
-    case COMPRESSION_LZNT1:
-      return lznt1_max_compressed_size (uncompressed_length);
-      break;
-    case COMPRESSION_XPRESS:
-      return xpress_max_compressed_size (uncompressed_length) + 4;
-      break;
-    case COMPRESSION_XPRESS_HUFF:
-      return uncompressed_length + 384 + (uncompressed_length / 100);
-      break;
-    default:
-      assert (false);
-  }
-}
-
-static size_t
-squash_ms_compress_get_uncompressed_size (SquashCodec* codec, const uint8_t* compressed, size_t compressed_length) {
-  switch (squash_codec_to_ms_compress_format (codec)) {
-    case COMPRESSION_LZNT1:
-      return lznt1_uncompressed_size ((const_bytes) compressed, compressed_length);
-      break;
-    case COMPRESSION_XPRESS:
-      return xpress_uncompressed_size ((const_bytes) compressed, compressed_length);
-      break;
-    default:
-      assert (false);
-  }
+  MSCompStatus status = ms_decompress (squash_ms_format_from_codec (codec),
+                                       compressed, compressed_length, decompressed, decompressed_length);
+  return squash_ms_status_to_squash_status (status);
 }
 
 SquashStatus
@@ -128,11 +205,13 @@ squash_plugin_init_codec (SquashCodec* codec, SquashCodecFuncs* funcs) {
   if (strcmp ("lznt1", name) == 0 ||
       strcmp ("xpress", name) == 0 ||
       strcmp ("xpress-huffman", name) == 0) {
-    funcs->get_max_compressed_size = squash_ms_compress_get_max_compressed_size;
-    funcs->decompress_buffer = squash_ms_compress_decompress_buffer;
-    funcs->compress_buffer = squash_ms_compress_compress_buffer;
-    if (strcmp ("xpress-huffman", name) != 0)
-      funcs->get_uncompressed_size = squash_ms_compress_get_uncompressed_size;
+    funcs->get_max_compressed_size = squash_ms_get_max_compressed_size;
+    funcs->decompress_buffer       = squash_ms_decompress_buffer;
+    funcs->compress_buffer         = squash_ms_compress_buffer;
+    if (strcmp ("lznt1", name) == 0) {
+      funcs->create_stream           = squash_ms_create_stream;
+      funcs->process_stream          = squash_ms_process_stream;
+    }
   } else {
     return SQUASH_UNABLE_TO_LOAD;
   }
