2038efe43b6e561f8a3c4f6d6975ae4533067955
Evan Nemerson <evan@nemerson.com>
2015-02-27 22:07:31 -0800

    build: add autotools-like interface for CMake
    Not only are most people used to the CLI provided by autotools-based
    build systems, it is also a lot nicer than CMake.
    
    https://github.com/quixdb/squash/issues/74

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-git submodule update --init --recursive
-cmake "$@" .
+if [ -e $(dirname $0)/.gitmodules ]; then
+    git submodule update --init --recursive
+fi
+
+$(dirname $0)/configure "$@"
