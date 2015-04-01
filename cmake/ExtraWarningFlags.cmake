827ffb5e4df7708b00c1a87a550b3bd6dd499e4e
Evan Nemerson <evan@nemerson.com>
2015-03-31 17:01:08 -0700

    Add many extra warning flags when compiling C++
    Signed-off-by: Evan Nemerson <evan@nemerson.com>

--- cmake/ExtraWarningFlags.cmake
+++ cmake/ExtraWarningFlags.cmake
@@ -2,8 +2,10 @@
 # https://git.gnome.org/browse/gnome-common/tree/macros2/gnome-compiler-flags.m4
 
 include (CheckCCompilerFlag)
+include (CheckCXXCompilerFlag)
 
-set (EXTRA_WARNING_FLAGS)
+set (EXTRA_WARNING_CFLAGS)
+set (EXTRA_WARNING_CXXFLAGS)
 
 foreach (flag
     -Wall
@@ -21,7 +23,6 @@ foreach (flag
     -Wcast-align
     -Wno-uninitialized
     -Wclobbered
-    # -Wdeclaration-after-statement
     -Wempty-body
     -Wformat-nonliteral
     -Wformat-security
@@ -47,7 +48,6 @@ foreach (flag
     -Wreturn-type
     -Wshadow
     -Wsign-compare
-    # -Wswitch-default
     -Wswitch-enum
     -Wsync-nand
     -Wtype-limits
@@ -60,15 +60,73 @@ foreach (flag
   string (REGEX REPLACE "[-=]+" "_" test_name "CFLAG_${flag}")
   CHECK_C_COMPILER_FLAG (${flag} ${test_name})
   if (${test_name})
-    set (EXTRA_WARNING_FLAGS "${EXTRA_WARNING_FLAGS} ${flag}")
+    set (EXTRA_WARNING_CFLAGS "${EXTRA_WARNING_CFLAGS} ${flag}")
   endif ()
 endforeach ()
 
-function (add_extra_warning_flags scope obj)
+foreach (flag
+    -Wall
+    -Werror=missing-prototypes
+    -Werror=implicit-function-declaration
+    -Werror=pointer-arith
+    -Werror=init-self
+    -Werror=format=2
+    -Werror=missing-include-dirs
+    -Wextra
+    -Waggregate-return
+    -Wcast-align
+    -Wno-uninitialized
+    -Wclobbered
+    -Wempty-body
+    -Wformat-nonliteral
+    -Wformat-security
+    -Wignored-qualifiers
+    -Winit-self
+    -Winline
+    -Winvalid-pch
+    -Wlogical-op
+    -Wmissing-declarations
+    -Wmissing-format-attribute
+    -Wmissing-include-dirs
+    -Wmissing-noreturn
+    -Wmissing-parameter-type
+    -Wno-missing-field-initializers
+    -Wno-strict-aliasing
+    -Wno-unused-parameter
+    -Wpacked
+    -Wpointer-arith
+    -Wredundant-decls
+    -Wreturn-type
+    -Wshadow
+    -Wsign-compare
+    -Wswitch-enum
+    -Wsync-nand
+    -Wtype-limits
+    -Wundef
+    -Wuninitialized
+    -Wwrite-strings
+)
+  string (REGEX REPLACE "[-=]+" "_" test_name "CXXFLAG_${flag}")
+  CHECK_CXX_COMPILER_FLAG (${flag} ${test_name})
+  if (${test_name})
+    set (EXTRA_WARNING_CXXFLAGS "${EXTRA_WARNING_CXXFLAGS} ${flag}")
+  endif ()
+endforeach ()
+
+function (add_extra_warning_cflags scope obj)
   get_property (existing ${scope} ${obj} PROPERTY COMPILE_FLAGS)
   if ("${existing}" STREQUAL "NOTFOUND")
-    set_property (${scope} ${obj} PROPERTY COMPILE_FLAGS "${EXTRA_WARNING_FLAGS}")
+    set_property (${scope} ${obj} PROPERTY COMPILE_FLAGS "${EXTRA_WARNING_CFLAGS}")
   else ()
-    set_property (${scope} ${obj} PROPERTY COMPILE_FLAGS "${existing} ${EXTRA_WARNING_FLAGS}")
+    set_property (${scope} ${obj} PROPERTY COMPILE_FLAGS "${existing} ${EXTRA_WARNING_CFLAGS}")
+  endif ()
+endfunction ()
+
+function (add_extra_warning_cxxflags scope obj)
+  get_property (existing ${scope} ${obj} PROPERTY COMPILE_FLAGS)
+  if ("${existing}" STREQUAL "NOTFOUND")
+    set_property (${scope} ${obj} PROPERTY COMPILE_FLAGS "${EXTRA_WARNING_CXXFLAGS}")
+  else ()
+    set_property (${scope} ${obj} PROPERTY COMPILE_FLAGS "${existing} ${EXTRA_WARNING_CXXFLAGS}")
   endif ()
 endfunction ()
