c6602601737fb50486b8d6d849e701e1febf4d0f
Evan Nemerson <evan@nemerson.com>
2015-04-20 02:14:51 -0700

    wflz: update to latest version, re-enable
    wfLZ was hosted on Google Code, but has now moved to GitHub (and git
    instead of subversion), so we can now drop the svn2github repo and use
    the new official one.  The new version also contains some fixes to
    avoid out-of-bounds accesses, which means we can re-enable the plugin
    by default.
    
    Signed-off-by: Evan Nemerson <evan@nemerson.com>

--- .configure-custom.sh
+++ .configure-custom.sh
@@ -3,7 +3,6 @@
 DISABLE_VARS="external|yes|FORCE_IN_TREE_DEPENDENCIES"
 DISABLE_FORCE_IN_TREE_DEPENDENCIES_DOC="force in-tree dependencies, even when a system library is available"
 
-ENABLE_VARS="doboz|yes|ENABLE_DOBOZ wflz|yes|ENABLE_WFLZ zling|yes|ENABLE_ZLING"
+ENABLE_VARS="doboz|yes|ENABLE_DOBOZ zling|yes|ENABLE_ZLING"
 ENABLE_DOBOZ_DOC="enable doboz plugin (disabled due to bugs)"
-ENABLE_WFLZ_DOC="enable wfLZ plugin (disabled due to bugs)"
 ENABLE_ZLING_DOC="enable zling plugin (disabled due to bugs)"
