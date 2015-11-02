quixdb/squash

commit 325b47d410402f59ca29fe44697bb700950fc8e2

Stay out of _ namespace
https://github.com/quixdb/squash/issues/139

squash/types-internal.h
@@ -38,17 +38,17 @@
 
 SQUASH_BEGIN_DECLS
 
-typedef SQUASH_TREE_HEAD(_SquashPluginTree, _SquashPlugin) SquashPluginTree;
-typedef SQUASH_TREE_HEAD(_SquashCodecTree, _SquashCodec) SquashCodecTree;
-typedef SQUASH_TREE_HEAD(_SquashCodecRefTree, _SquashCodecRef) SquashCodecRefTree;
+typedef SQUASH_TREE_HEAD(SquashPluginTree_, _SquashPlugin) SquashPluginTree;
+typedef SQUASH_TREE_HEAD(SquashCodecTree_, _SquashCodec) SquashCodecTree;
+typedef SQUASH_TREE_HEAD(SquashCodecRefTree_, _SquashCodecRef) SquashCodecRefTree;
 
-struct _SquashContext {
+struct SquashContext_ {
   SquashPluginTree plugins;
   SquashCodecRefTree codecs;
   SquashCodecRefTree extensions;
 };
 
-struct _SquashPlugin {
+struct SquashPlugin_ {
   SquashContext* context;
 
   char* name;
@@ -63,10 +63,10 @@ struct _SquashPlugin {
 
   SquashCodecTree codecs;
 
-  SQUASH_TREE_ENTRY(_SquashPlugin) tree;
+  SQUASH_TREE_ENTRY(SquashPlugin_) tree;
 };
 
-struct _SquashCodec {
+struct SquashCodec_ {
   SquashPlugin* plugin;
 
   char* name;
@@ -76,16 +76,16 @@ struct _SquashCodec {
   bool initialized;
   SquashCodecImpl impl;
 
-  SQUASH_TREE_ENTRY(_SquashCodec) tree;
+  SQUASH_TREE_ENTRY(SquashCodec_) tree;
 };
 
-typedef struct _SquashCodecRef {
+typedef struct SquashCodecRef_ {
   SquashCodec* codec;
 
-  SQUASH_TREE_ENTRY(_SquashCodecRef) tree;
+  SQUASH_TREE_ENTRY(SquashCodecRef_) tree;
 } SquashCodecRef;
 
-typedef struct _SquashBuffer {
+typedef struct SquashBuffer_ {
   uint8_t* data;
   size_t size;
   size_t allocated;

squash/buffer-stream-internal.h
@@ -38,12 +38,12 @@ SQUASH_BEGIN_DECLS
 #  define SQUASH_BUFFER_STREAM_BUFFER_SIZE (4096 - sizeof (SquashSList))
 #endif
 
-typedef struct _SquashBufferStreamSList {
+typedef struct SquashBufferStreamSList_ {
   SquashSList base;
   uint8_t data[SQUASH_BUFFER_STREAM_BUFFER_SIZE];
 } SquashBufferStreamSList;
 
-typedef struct _SquashBufferStream {
+typedef struct SquashBufferStream_ {
   SquashStream base_object;
 
   SquashBuffer* input;

squash/codec-internal.h
@@ -52,8 +52,8 @@ SquashStatus            squash_codec_decompress_to_buffer    (SquashCodec* codec
                                                               uint8_t compressed[SQUASH_ARRAY_PARAM(compressed_size)],
                                                               SquashOptions* options);
 
-SQUASH_TREE_PROTOTYPES(_SquashCodec, tree)
-SQUASH_TREE_DEFINE(_SquashCodec, tree)
+SQUASH_TREE_PROTOTYPES(SquashCodec_, tree)
+SQUASH_TREE_DEFINE(SquashCodec_, tree)
 
 SQUASH_END_DECLS
 

squash/codec.c
@@ -51,7 +51,7 @@
  */
 
 /**
- * @struct _SquashCodecImpl
+ * @struct SquashCodecImpl_
  * @brief Function table for plugins
  *
  * This struct should only be used from within a plugin.
@@ -62,17 +62,17 @@
  */
 
 /**
- * @var _SquashCodecImpl::info
+ * @var SquashCodecImpl_::info
  * @brief Capability information about the codec
  */
 
 /**
- * @var _SquashCodecImpl::options
+ * @var SquashCodecImpl_::options
  * @brief options which may bo passed to the codec to modify its operation
  */
 
 /**
- * @var _SquashCodecImpl::create_stream
+ * @var SquashCodecImpl_::create_stream
  * @brief Create a new %SquashStream.
  *
  * @param codec The codec.
@@ -84,7 +84,7 @@
  */
 
 /**
- * @var _SquashCodecImpl::process_stream
+ * @var SquashCodecImpl_::process_stream
  * @brief Process a %SquashStream.
  *
  * @param stream The stream.
@@ -95,7 +95,7 @@
  */
 
 /**
- * @var _SquashCodecImpl::splice
+ * @var SquashCodecImpl_::splice
  * @brief Splice.
  *
  * @param options Options to use
@@ -107,7 +107,7 @@
  */
 
 /**
- * @var _SquashCodecImpl::get_uncompressed_size
+ * @var SquashCodecImpl_::get_uncompressed_size
  * @brief Get the buffer's uncompressed size.
  *
  * @param codec The codec.
@@ -119,7 +119,7 @@
  */
 
 /**
- * @var _SquashCodecImpl::get_max_compressed_size
+ * @var SquashCodecImpl_::get_max_compressed_size
  * @brief Get the maximum compressed size.
  *
  * @param codec The codec.
@@ -131,7 +131,7 @@
  */
 
 /**
- * @var _SquashCodecImpl::decompress_buffer
+ * @var SquashCodecImpl_::decompress_buffer
  * @brief Decompress a buffer.
  *
  * @param codec The codec.
@@ -146,7 +146,7 @@
  */
 
 /**
- * @var _SquashCodecImpl::compress_buffer
+ * @var SquashCodecImpl_::compress_buffer
  * @brief Compress a buffer.
  *
  * @param codec The codec.
@@ -161,7 +161,7 @@
  */
 
 /**
- * @var _SquashCodecImpl::compress_buffer_unsafe
+ * @var SquashCodecImpl_::compress_buffer_unsafe
  * @brief Compress a buffer.
  *
  * Plugins implementing this function can be sure that @a compressed
@@ -180,42 +180,42 @@
  */
 
 /**
- * @var _SquashCodecImpl::_reserved1
+ * @var SquashCodecImpl_::_reserved1
  * @brief Reserved for future use.
  */
 
 /**
- * @var _SquashCodecImpl::_reserved2
+ * @var SquashCodecImpl_::_reserved2
  * @brief Reserved for future use.
  */
 
 /**
- * @var _SquashCodecImpl::_reserved3
+ * @var SquashCodecImpl_::_reserved3
  * @brief Reserved for future use.
  */
 
 /**
- * @var _SquashCodecImpl::_reserved4
+ * @var SquashCodecImpl_::_reserved4
  * @brief Reserved for future use.
  */
 
 /**
- * @var _SquashCodecImpl::_reserved5
+ * @var SquashCodecImpl_::_reserved5
  * @brief Reserved for future use.
  */
 
 /**
- * @var _SquashCodecImpl::_reserved6
+ * @var SquashCodecImpl_::_reserved6
  * @brief Reserved for future use.
  */
 
 /**
- * @var _SquashCodecImpl::_reserved7
+ * @var SquashCodecImpl_::_reserved7
  * @brief Reserved for future use.
  */
 
 /**
- * @var _SquashCodecImpl::_reserved8
+ * @var SquashCodecImpl_::_reserved8
  * @brief Reserved for future use.
  */
 
@@ -255,7 +255,7 @@ squash_codec_extension_compare (SquashCodec* a, SquashCodec* b) {
  */
 
 /**
- * @struct _SquashCodec
+ * @struct SquashCodec_
  * @brief A compression/decompression codec
  */
 

squash/codec.h
@@ -59,7 +59,7 @@ typedef SquashStatus (*SquashWriteFunc) (size_t* data_size,
                                          const uint8_t data[SQUASH_ARRAY_PARAM(*data_size)],
                                          void* user_data);
 
-struct _SquashCodecImpl {
+struct SquashCodecImpl_ {
   SquashCodecInfo           info;
 
   const SquashOptionInfo*   options;

squash/context-internal.h
@@ -37,8 +37,8 @@ SQUASH_BEGIN_DECLS
 SQUASH_NONNULL(1, 2)
 void            squash_context_add_codec     (SquashContext* context, SquashCodec* codec);
 
-SQUASH_TREE_PROTOTYPES(_SquashCodecRef, tree)
-SQUASH_TREE_DEFINE(_SquashCodecRef, tree)
+SQUASH_TREE_PROTOTYPES(SquashCodecRef_, tree)
+SQUASH_TREE_DEFINE(SquashCodecRef_, tree)
 
 SQUASH_END_DECLS
 

squash/context.c
@@ -74,7 +74,7 @@ static char* squash_strndup(const char* s, size_t n);
  */
 
 /**
- * @struct _SquashContext
+ * @struct SquashContext_
  * @brief Context for all Squash operations.
  */
 
@@ -88,7 +88,7 @@ squash_context_get_codec_ref (SquashContext* context, const char* codec) {
 
   key_codec.name = (char*) codec;
 
-  return SQUASH_TREE_FIND (&(context->codecs), _SquashCodecRef, tree, &key);
+  return SQUASH_TREE_FIND (&(context->codecs), SquashCodecRef_, tree, &key);
 }
 
 static SquashCodecRef*
@@ -101,7 +101,7 @@ squash_context_get_codec_ref_from_extension (SquashContext* context, const char*
 
   key_codec.extension = (char*) extension;
 
-  return SQUASH_TREE_FIND (&(context->extensions), _SquashCodecRef, tree, &key);
+  return SQUASH_TREE_FIND (&(context->extensions), SquashCodecRef_, tree, &key);
 }
 
 /**
@@ -197,7 +197,7 @@ squash_context_get_plugin (SquashContext* context, const char* plugin) {
 
   plugin_dummy.name = (char*) plugin;
 
-  return SQUASH_TREE_FIND (&(context->plugins), _SquashPlugin, tree, &plugin_dummy);
+  return SQUASH_TREE_FIND (&(context->plugins), SquashPlugin_, tree, &plugin_dummy);
 }
 
 /**
@@ -233,11 +233,11 @@ squash_context_add_plugin (SquashContext* context, char* name, char* directory) 
 
   plugin_dummy.name = name;
 
-  plugin = SQUASH_TREE_FIND (&(context->plugins), _SquashPlugin, tree, &plugin_dummy);
+  plugin = SQUASH_TREE_FIND (&(context->plugins), SquashPlugin_, tree, &plugin_dummy);
   if (plugin == NULL) {
     plugin = squash_plugin_new (name, directory, context);
 
-    SQUASH_TREE_INSERT(&(context->plugins), _SquashPlugin, tree, plugin);
+    SQUASH_TREE_INSERT(&(context->plugins), SquashPlugin_, tree, plugin);
   } else {
     free (name);
     free (directory);
@@ -274,7 +274,7 @@ squash_context_add_codec (SquashContext* context, SquashCodec* codec) {
     codec_ref = (SquashCodecRef*) malloc (sizeof (SquashCodecRef));
     codec_ref->codec = codec;
     SQUASH_TREE_ENTRY_INIT(codec_ref->tree);
-    SQUASH_TREE_INSERT (&(context->codecs), _SquashCodecRef, tree, codec_ref);
+    SQUASH_TREE_INSERT (&(context->codecs), SquashCodecRef_, tree, codec_ref);
   } else if (codec->priority > codec_ref->codec->priority) {
     /* Switch the existing context codec's details to this one */
     codec_ref->codec = codec;
@@ -286,7 +286,7 @@ squash_context_add_codec (SquashContext* context, SquashCodec* codec) {
       codec_ref = (SquashCodecRef*) malloc (sizeof (SquashCodecRef));
       codec_ref->codec = codec;
       SQUASH_TREE_ENTRY_INIT(codec_ref->tree);
-      SQUASH_TREE_INSERT (&(context->extensions), _SquashCodecRef, tree, codec_ref);
+      SQUASH_TREE_INSERT (&(context->extensions), SquashCodecRef_, tree, codec_ref);
     } else if (codec->priority > codec_ref->codec->priority) {
       codec_ref->codec = codec;
     }
@@ -296,7 +296,7 @@ squash_context_add_codec (SquashContext* context, SquashCodec* codec) {
 /**
  * @private
  */
-typedef struct _SquashCodecsFileParser {
+typedef struct SquashCodecsFileParser_ {
   SquashPlugin* plugin;
   SquashCodec* codec;
 } SquashCodecsFileParser;
@@ -568,12 +568,12 @@ squash_context_find_plugins (SquashContext* context) {
  */
 void
 squash_context_foreach_plugin (SquashContext* context, SquashPluginForeachFunc func, void* data) {
-  SQUASH_TREE_FORWARD_APPLY(&(context->plugins), _SquashPlugin, tree, func, data);
+  SQUASH_TREE_FORWARD_APPLY(&(context->plugins), SquashPlugin_, tree, func, data);
 }
 
 static void
 squash_context_foreach_codec_ref (SquashContext* context, void(*func)(SquashCodecRef*, void*), void* data) {
-  SQUASH_TREE_FORWARD_APPLY(&(context->codecs), _SquashCodecRef, tree, func, data);
+  SQUASH_TREE_FORWARD_APPLY(&(context->codecs), SquashCodecRef_, tree, func, data);
 }
 
 /**

squash/file.c
@@ -49,7 +49,7 @@
  * @cond INTERNAL
  */
 
-struct _SquashFile {
+struct SquashFile_ {
   FILE* fp;
   mtx_t mtx;
   bool eof;

commit 3ee52b6087c52347636cd953431825a0c59853d1

Clean up warnings caused by 325b47d4
Not all references to were switch, which caused lots of warnings.
The problem was sed s/…/…/ instead of s/…/…/g, and not having -Werror
in my CFLAGS.

squash/types-internal.h
@@ -38,9 +38,9 @@
 
 SQUASH_BEGIN_DECLS
 
-typedef SQUASH_TREE_HEAD(SquashPluginTree_, _SquashPlugin) SquashPluginTree;
-typedef SQUASH_TREE_HEAD(SquashCodecTree_, _SquashCodec) SquashCodecTree;
-typedef SQUASH_TREE_HEAD(SquashCodecRefTree_, _SquashCodecRef) SquashCodecRefTree;
+typedef SQUASH_TREE_HEAD(SquashPluginTree_, SquashPlugin_) SquashPluginTree;
+typedef SQUASH_TREE_HEAD(SquashCodecTree_, SquashCodec_) SquashCodecTree;
+typedef SQUASH_TREE_HEAD(SquashCodecRefTree_, SquashCodecRef_) SquashCodecRefTree;
 
 struct SquashContext_ {
   SquashPluginTree plugins;