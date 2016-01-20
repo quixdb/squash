652184dd0c3e81bdb23130a49ca2f9ac4cccc9fb
Evan Nemerson <evan@nemerson.com>
2016-01-19 16:26:44 -0800

    tests/threads: add CPU count detection code
    This should reproduce the old behavior of the tests, which AppVeyor
    could manage, where we spawned n_cpus threads.
    
    Also, restore the timeout on appveyor to 300 seconds.

--- tests/threads.c
+++ tests/threads.c
@@ -1,6 +1,45 @@
+#if defined(_WIN32)
+#  include <windows.h>
+#else
+#  include <unistd.h>
+#endif
+
 #include "test-squash.h"
 
 #include "../squash/tinycthread/source/tinycthread.h"
+
+static unsigned int
+squash_test_cpu_count (void) {
+  static unsigned int count = 0;
+
+  if (SQUASH_UNLIKELY (count == 0)) {
+    unsigned int c = 0;
+
+#if defined(_WIN32)
+    /* TODO: Figure out how Windows programmers ever get anything done. */
+    PDWORD lpProcessAffinityMask;
+    PDWORD lpSystemAffinityMask;
+
+    if (!GetProcessAffinityMask (GetCurrentProcess (), &lpProcessAffinityMask, &lpSystemAffinityMask)) {
+      DWORD ec = GetLastError ();
+      count = 1;
+    } else {
+      for (unsigned int i = 0 ; lpProcessAffinityMask != 0 ; lpProcessAffinityMask >>= 1) {
+        if (lpProcessAffinityMask & 1)
+          c++;
+      }
+    }
+#elif defined(_SC_NPROCESSORS_ONLN)
+    c = (unsigned int) sysconf (_SC_NPROCESSORS_ONLN);
+#else
+#warning No idea how to get the number of CPUs
+#endif
+
+    count = (c == 0) ? 1 : c;
+  }
+
+  return count;
+}
 
 static int
 compress_buffer_thread_func (SquashCodec* codec) {
@@ -37,8 +76,8 @@ squash_test_threads_buffer(MUNIT_UNUSED const MunitParameter params[], void* use
   munit_assert_non_null(user_data);
   SquashCodec* codec = (SquashCodec*) user_data;
 
-  const unsigned int n_threads = 3;
-  thrd_t threads[3];
+  const unsigned int n_threads = squash_test_cpu_count ();
+  thrd_t* threads = munit_newa(thrd_t, n_threads);
 
   for (unsigned int i = 0 ; i < n_threads ; i++) {
     const int r = thrd_create(&(threads[i]), (thrd_start_t) compress_buffer_thread_func, codec);
@@ -49,9 +88,13 @@ squash_test_threads_buffer(MUNIT_UNUSED const MunitParameter params[], void* use
     int retval;
     const int r = thrd_join(threads[i], &retval);
     munit_assert_cmp_int (r, ==, thrd_success);
-    if (MUNIT_UNLIKELY(retval != MUNIT_OK))
+    if (MUNIT_UNLIKELY(retval != MUNIT_OK)) {
+      free (threads);
       return (MunitResult) retval;
+    }
   }
+
+  free (threads);
 
   return MUNIT_OK;
 }
